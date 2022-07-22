8a9fbc2171619f61251d48467462d08701de76b1
misterazi <azrimdkasim@gmail.com>
2022-07-22 12:15:39 +0800

    Task 3 Done
    Hardcoded with user id

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,27 @@
 import "./App.css";
+import React, { useState, useEffect } from "react";
+import { data } from "./data";
+import Axios from "axios";
 
 function App() {
+  const [walletList, setWalletList] = useState(data);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="Dashboard">
+      <h2>Dashboard</h2>
+      {walletList
+        .filter((wallet) => {
+          return wallet.user_id === 1;
+        })
+        .map((wallet) => {
+          return (
+            <div className="Wallet">
+              <label>UserID: {wallet.user_id}</label>
+              <label>WalletID: {wallet.id}</label>
+              <label>{wallet.name}</label>
+            </div>
+          );
+        })}
     </div>
   );
 }
